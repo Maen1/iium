49e92c5394be39194d5a3ef8c99e234b11ec4108
Maen1 <mk.maen93@gmail.com>
2019-04-17 20:17:51 +0800

    drop columns

--- 6/bda3303/project/script.py
+++ 6/bda3303/project/script.py
@@ -4,8 +4,20 @@ from sklearn.cluster import KMeans
 X = np.array([[1, 2], [1, 4], [1, 0],
     [10, 2], [10, 4], [10, 0]])
 
+## data cleaning
 df = pd.read_csv('./food_coded.csv')
 print(df.head())
+
+drop_cols = ['comfort_food', 'comfort_food_reasons',
+'diet_current', 'eating_changes', 'father_profession',
+'fav_cuisine', 'food_childhood','healthy_meal', 'ideal_diet',
+'meals_dinner_friend','mother_profession','type_sports']
+
+df.drop(drop_cols, inplace=True, axis=1)
+print(df.head())
+
+
+## end data cleaning
 
 kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
 print(kmeans.labels_)
